d07c4613fb946bfeee1508cdd78020294db69b37
Renato Dias <renatocardosodias@gmail.com>
2022-08-31 14:10:37 -0300

    Add @IBOutlett on UITextField tfTotalPassword Add @IBOutlett on UITextField tfNumberOfCaracters Add @IBOutlett on UITextField swLetters Add @IBOutlett on UITextField swCapitalLetteres Add @IBOutlett on UITextField swNumbers Add @IBOutlett on UITextField swSpecialCaracters

--- SuperSenha/ViewController.swift
+++ SuperSenha/ViewController.swift
@@ -9,6 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tfTotalPasswords: UITextField!
+    @IBOutlet weak var tfNumbersOfCaracters: UITextField!
+    @IBOutlet weak var swLetters: UISwitch!
+    @IBOutlet weak var seCapitalLetters: UISwitch!
+    @IBOutlet weak var swNumbers: UISwitch!
+    @IBOutlet weak var swSpecialCaracters: UISwitch!
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
