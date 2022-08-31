6d52173b82a099a7bbb7a56a44ad6862053618c8
Renato Dias <renatocardosodias@gmail.com>
2022-08-31 14:15:14 -0300

    Criando variáveis que serão recebidos os dados

--- SuperSenha/PasswordViewController.swift
+++ SuperSenha/PasswordViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 class PasswordViewController: UIViewController {
 
     @IBOutlet weak var tvPassword: UITextView!
+    
+    //Criando caracteres
+    var numberOfCaracteres: Int = 10
+    var numberOfPassword: Int = 1
+    var useLetters: Bool!
+    var useCapitalLetters: Bool!
+    var useNumbers: Bool!
+    var userSpecialCaracteres: Bool!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
