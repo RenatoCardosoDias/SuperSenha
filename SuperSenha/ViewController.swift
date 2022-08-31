8f0938eae9000bb0db47087438b07aae3ddb6285
Renato Dias <renatocardosodias@gmail.com>
2022-08-31 17:43:43 -0300

    PASSANDO INFORMAÇÕES DE UMA VIEWCONTROLLER PARA OUTRA
    antes da tela ser apresentada, carregar as informações
    esse metodo é executado momentos antes da segue apresentar a nova tela
    
    CRIAR UM OBJETO QUE SEJA REFERENCIA A VIEW CONTROLER QUE IRA MOSTRAR
    
    //fazendo com que o finalize o modo edição,qualquer TextField que esteja seleciondao ele vai ficar desselecioando,vai perder o foco automaticamente o teclado vai desaparecer
            view.endEditing(true)

--- SuperSenha/ViewController.swift
+++ SuperSenha/ViewController.swift
@@ -25,10 +25,32 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    //PASSANDO INFORMAÇÕES DE UMA VIEWCONTROLLER PARA OUTRA
+    
+    
     //antes da tela ser apresentada, carregar as informações
     //esse metodo é executado momentos antes da segue apresentar a nova tela
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        <#code#>
+        //nesse momento consigo recuperar a tela e alimentar ela
+        
+        //CRIAR UM OBJETO QUE SEJA REFERENCIA A VIEW CONTROLER QUE IRA MOSTRAR
+        let passwordViewController = segue.destination as! PasswordViewController
+        //tendo acesso a todas a variaveis criada no arquivo PasswordViewController.swift
+        
+        
+        if let numberOfPasswords = Int(tfTotalPasswords.text!){
+            passwordViewController.numberOfPassword = numberOfPasswords
+        }
+        if let numberOfCharacteres = Int(tfNumbersOfCaracters.text!){
+            passwordViewController.numberOfCaracteres = numberOfCharacteres
+        }
+        passwordViewController.useLetters = swLetters.isOn
+        passwordViewController.useCapitalLetters = seCapitalLetters.isOn
+        passwordViewController.useNumbers = swNumbers.isOn
+        passwordViewController.userSpecialCaracteres = swSpecialCaracters.isOn
+        //fazendo com que o finalize o modo edição,qualquer TextField que esteja seleciondao ele vai ficar desselecioando,vai perder o foco automaticamente o teclado vai desaparecer
+        view.endEditing(true)
+        
     }
 
 
