8ea1e600e24f8f7035be16dfe64cfa92e07b1fd7
Renato Dias <renatocardosodias@gmail.com>
2022-08-31 14:17:12 -0300

    Add @IBAction - gernerate para criar os números aleatórios

--- SuperSenha/PasswordViewController.swift
+++ SuperSenha/PasswordViewController.swift
@@ -29,14 +29,10 @@ class PasswordViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func generate(_ sender: UIButton) {
+        
+        
     }
-    */
+    
 
 }
