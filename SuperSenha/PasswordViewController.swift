9b9f0ebb7c79714adfb3d83f0eb9a6d9e51daa7c
Renato Dias <renatocardosodias@gmail.com>
2022-08-31 14:11:22 -0300

    Add Cocoatouch PasswordViewController e ligado a UIViewController

---
+++ SuperSenha/PasswordViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PasswordViewController.swift
+//  SuperSenha
+//
+//  Created by Renato on 31/08/22.
+//
+
+import UIKit
+
+class PasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
